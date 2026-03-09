ae282a971ae31faba420c5ddaa7bec2a50dfeff4
kiettt164 <trankiet164@gmail.com>
2026-03-09 14:07:18 +0700

    fix: PR comment - avoid backtick in template literal v3

--- phase8-application-v3/common/auth.py
+++ phase8-application-v3/common/auth.py
@@ -1,5 +1,7 @@
 # Sửa file trong common, services, phase4, phase8 hoặc k8s-chatbot
 # Ví dụ: thêm 1 dòng comment vào common/__init__.py hoặc file bất kỳimport os
+# Sửa file trong common, services, phase4, phase8 hoặc k8s-chatbot
+# Ví dụ: thêm 1 dòng comment vào common/__init__.py hoặc file bất kỳimport os
 from passlib.context import CryptContext
 
 # Giảm rounds (mặc định 10) nếu CPU cao; 12 = an toàn hơn nhưng ~2x chậm
